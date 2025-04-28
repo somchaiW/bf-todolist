94bf8fdeaf70676450f1e00848146b8296194184
TripleJay008 <totsakan34@gmail.com>
2025-04-28 16:11:24 +0700

    mock data

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -7,6 +7,132 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
+
+    public function add_task(Request $request)
+    {
+        
+        $task = [
+            'id' => 1,
+            'name' => $request->name,
+            'description' => $request->description,
+            'date' => $request->date,
+            'time' => $request->time,
+            'priority' => $request->priority,
+            'category' => $request->category,
+            'status' => false, 
+        ];
+
+        return response()->json([
+            'status' => 'success',
+            'status_code' => 200,
+            'data' => [
+                'task' => $task,
+            ]
+        ], 200);
+    }
+    public function get_priority(){
+        $priority = [
+            [
+                'id'=>1,
+                'name'=>'1'
+            ],
+            [
+                'id'=>2,
+                'name'=>'2'
+            ],
+            [
+                'id'=>3,
+                'name'=>'3'
+            ]
+        ];return response()->json([
+            'status' => 'success',
+            'status_code' => 200,
+            'data' => [
+                'priority' => $priority,
+            ]
+        ], 200);
+    }
+
+    public function get_category(){
+        $category = [
+            [
+                'id'=>1,
+                'name'=>'Grocery',
+                'image'=>'Grocery',
+                'color'=>'0xFFccff80'
+           
+            ],
+            [
+                'id'=>2,
+                'name'=>'Work',
+                'image'=>'Work',
+                'color'=>'0xFFccff9680'
+            ],
+            [
+                'id'=>3,
+                'name'=>'Sport',
+                'image'=>'Sport',
+                'color'=>'0xFF8ffff'
+            ],
+            [
+                'id'=>4,
+                'name'=>'Design',
+                'image'=>'Design',
+                'color'=>'0xFF80ffd9'
+            ],
+            [
+                'id'=>5,
+                'name'=>'University',
+                'image'=>'University',
+                'color'=>'0xFF809cff'
+            ],
+            [
+                'id'=>6,
+                'name'=>'Social',
+                'image'=>'Social',
+                'color'=>'0xFFff80eb'
+            ],
+            [
+                'id'=>7,
+                'name'=>'Music',
+                'image'=>'Music',
+                'color'=>'0xFFfc80ff'
+            ],
+            [
+                'id'=>8,
+                'name'=>'Health',
+                'image'=>'Health',
+                'color'=>'0xFF80ffa3'
+            ],
+            [
+                'id'=>9,
+                'name'=>'Movie',
+                'image'=>'Movie',
+                'color'=>'0xFF80d1ff'
+            ],
+            [
+                'id'=>10,
+                'name'=>'Home',
+                'image'=>'Home',
+                'color'=>'0xFFffcc80'
+            ],
+            [
+                'id'=>11,
+                'name'=>'None',
+                'image'=>'None',
+                'color'=>'0xFF8875ff'
+            ],
+
+        ]; return response()->json([
+            'status' => 'success',
+            'status_code' => 200,
+            'data' => [
+                'category' => $category,
+            ]
+        ], 200);
+    }
+
+
     public function get_list_task()
     {
         $tasks = [
@@ -58,6 +184,36 @@ class TaskController extends Controller
             'status_code' => 200,
             'data' => [
                 'text' => 'update success'
+            ]
+        ], 200);
+    }
+    public function change_name(Request $request, $id)
+    {
+        return response()->json([
+            'status' => 'success',
+            'status_code' => 200,
+            'data' => [
+                'text' => 'edit name success'
+            ]
+        ], 200);
+    }
+    public function change_pass(Request $request, $id)
+    {
+        return response()->json([
+            'status' => 'success',
+            'status_code' => 200,
+            'data' => [
+                'text' => 'edit pass success'
+            ]
+        ], 200);
+    }
+    public function change_image(Request $request, $id)
+    {
+        return response()->json([
+            'status' => 'success',
+            'status_code' => 200,
+            'data' => [
+                'text' => 'edit profile success'
             ]
         ], 200);
     }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\AppController;
+use App\Http\Controllers\Api\TaskController;
 
 
 Route::apiResource('posts', PostController::class);
@@ -11,10 +12,17 @@ Route::apiResource('posts', PostController::class);
 
 Route::post('/register', [AppController::class, 'register']);
 Route::get('/ini_app',[AppController::class, 'ini_app']);
-Route::post('/login',[AppController::class,'login']);
+Route::post('/login',[AppController::class, 'login']);
 Route::get('/get_user/{id}',[AppController::class,'get_user']);
 Route::get('/get_list_task',[TaskController::class, 'get_list_task']);
 Route::put('/get_task/{id}',[TaskController::class, 'update_task']);
+Route::post('/add_task',[TaskController::class, 'add_task']);
+Route::get('/get_category',[TaskController::class, 'get_category']);
+Route::get('/get_priority',[TaskController::class, 'get_priority']);
+Route::put('/change_name/{id}',[TaskController::class, 'change_name']);
+Route::put('/change_pass/{id}',[TaskController::class, 'change_pass']);
+Route::put('/change_image/{id}',[TaskController::class, 'change_image']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
